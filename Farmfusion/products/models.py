46434994022ca58f2adfc5c430e34f4c495ad1e1
Your Name <fardinfarhan79x@gmail.com>
2024-11-30 01:15:23 +0600

    created model for Review

--- Farmfusion/products/models.py
+++ Farmfusion/products/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 class Product(models.Model):
     name = models.CharField(max_length=100)
@@ -8,3 +8,15 @@ class Product(models.Model):
     image = models.ImageField(upload_to='pics', null=True, blank=True)
     def __str__(self):
         return str(self.name)
+
+
+
+class Review(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='reviews')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.IntegerField(choices=[(i, i) for i in range(1, 6)])  # Rating from 1 to 5
+    comment = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.product.name} - {self.user.username} ({self.rating})"
