98d1d51e30c420441081207c09ff61ab1538d83d
Your Name <fardinfarhan79x@gmail.com>
2024-11-30 01:20:57 +0600

    created a Review function for view Review

--- Farmfusion/products/views.py
+++ Farmfusion/products/views.py
@@ -28,3 +28,47 @@ def update_product(request,p_id):
 def delete_product(request,p_id):
     Product.objects.get(pk=p_id).delete()
     return HttpResponse('Success')
+
+
+
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Product, Review
+from .forms import ReviewForm
+from django.contrib.auth.decorators import login_required
+from django.db.models import Avg
+
+def product_detail(request,p_id):
+    product = get_object_or_404(Product, pk=p_id)
+    reviews = product.reviews.all()
+    average_rating= reviews.aggregate(Avg('rating'))['rating__avg'] if reviews else 0
+    # average_rating = reviews.aggregate(Avg('rating'))['rating__avg'] if reviews() else 0
+    if request.method == 'POST':
+        if request.user.is_authenticated:
+            form = ReviewForm(request.POST)
+            if form.is_valid():
+                review = form.save(commit=False)
+                review.product = product
+                review.user = request.user
+                review.save()
+                return redirect('product_detail', p_id=product.id)
+        else:
+            return redirect('login')
+    else:
+        form = ReviewForm()
+
+    return render(request, 'components/product_detail.html', {
+        'product': product,
+        'reviews': reviews,
+        'form': form,
+        'average_rating': average_rating
+    }
+
+                  )
+
+
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+
+def redirect_to_product(request, product_id):
+    return HttpResponseRedirect(reverse('product_detail', args=[product_id]))
+
