8d2be2ee6ce6f7466752852238e92dccde5e3643
Your Name <fardinfarhan79x@gmail.com>
2024-11-30 01:18:22 +0600

    created form for Review

--- Farmfusion/products/forms.py
+++ Farmfusion/products/forms.py
@@ -5,3 +5,12 @@ class ProductForm(forms.ModelForm):
     class Meta :
         model = Product
         fields = ['name','price','quantity','image']
+
+
+
+from .models import Review
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        fields = ['rating', 'comment']
